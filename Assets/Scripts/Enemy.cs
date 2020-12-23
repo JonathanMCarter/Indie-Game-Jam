6c0f8afc79f81e1127c7a6a75054c56a9203b55c
Jonathan <jonathan@carter.games>
2020-12-23 13:01:58 +0000

    Emeny Pathfinding Improvements Pt.1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,15 +13,17 @@ namespace CarterGames.NoPresentsForYou
     {
         [SerializeField] private GameObject player;
         [SerializeField] private List<Moves> assignedMoves;
+        [SerializeField] private Moves[] pathFinding;
 
         private TurnController tc;
         private GameObject toMoveTo;
-
+        private RaycastHit _hit;
 
         private void Start()
         {
             player = GameObject.FindGameObjectWithTag("Player").gameObject;
             tc = GameObject.FindGameObjectWithTag("TurnController").GetComponent<TurnController>();
+            pathFinding = new Moves[4];
         }
 
 
@@ -45,112 +47,67 @@ namespace CarterGames.NoPresentsForYou
         }
 
 
-        private Moves ChooseDirection()
+        private char IsPositive(float value)
         {
-            Vector3 _check = player.transform.position - transform.position;
-
-            if (_check.x > _check.z)
-            {
-                if ((_check.z * -1) < _check.x)
-                {
-                    if (_check.x > .1f)
-                    {
-                        Debug.Log("Enemy - Go Left");
-                        return Moves.Left;
-                    }
-                    else if (_check.x < -.1f)
-                    {
-                        Debug.Log("Enemy - Go Right");
-                        return Moves.Right;
-                    }
-                }
-                else if (_check.z < -.1f)
-                {
-                    Debug.Log("Enemy - Go Up");
-                    return Moves.Up;
-                }
-            }
-            else if (_check.z > _check.x)
-            {
-                if ((_check.x * -1) < _check.z)
-                {
-                    if (_check.z > .1f)
-                    {
-                        Debug.Log("Enemy - Go Down");
-                        return Moves.Down;
-                    }
-                    else if (_check.z < -.1f)
-                    {
-                        Debug.Log("Enemy - Go Up");
-                        return Moves.Up;
-                    }
-                }
-                else if (_check.x < -.1f)
-                {
-                    Debug.Log("Enemy - Go Right");
-                    return Moves.Right;
-                }
-            }
-
-            return Moves.Defend;
+            if (value > 0)
+                return 'p';
+            else if (value < 0)
+                return 'n';
+            else
+                return 'z';
         }
 
-
-        private void MoveEnemy()
+        /// <summary>
+        /// Legit using my Y1-PF-AE2 Pathfinding Solution, as it still works and my pervious attempt had a few troubles with walls/holes.
+        /// </summary>
+        /// <returns></returns>
+        private void ChooseDirection()
         {
-            if (assignedMoves == null || assignedMoves.Count.Equals(0))
+            Vector3 _check = player.transform.position - transform.position;
+            bool isZ = false;
+
+
+            if (_check.z > _check.x)
+                isZ = true;
+
+
+            float _testX = IsPositive(_check.x);
+            float _testZ = IsPositive(_check.z);
+
+
+            if (isZ)
             {
-                RaycastHit _hit;
-
-                switch (ChooseDirection())
+                switch (_testZ)
                 {
-                    case Moves.Up:
-
-                        if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
-                        {
-                            if (_hit.collider.CompareTag("Tile"))
-                            {
-                                toMoveTo = _hit.collider.gameObject;
-                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 180, 0);
-                            }
-                        }
-
+                    case 'p':
+                        pathFinding[0] = Moves.Down;
+                        pathFinding[3] = Moves.Up;
                         break;
-                    case Moves.Down:
-
-                        if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
-                        {
-                            if (_hit.collider.CompareTag("Tile"))
-                            {
-                                toMoveTo = _hit.collider.gameObject;
-                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
-                            }
-                        }
-
+                    case 'n':
+                        pathFinding[0] = Moves.Up;
+                        pathFinding[3] = Moves.Down;
                         break;
-                    case Moves.Left:
-
-                        if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
-                        {
-                            if (_hit.collider.CompareTag("Tile"))
-                            {
-                                toMoveTo = _hit.collider.gameObject;
-                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 90, 0);
-                            }
-                        }
-
+                    case 'z':
+                        pathFinding[0] = Moves.Down;
+                        pathFinding[3] = Moves.Up;
                         break;
-                    case Moves.Right:
+                    default:
+                        break;
+                }
 
-                        if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
-                        {
-                            if (_hit.collider.CompareTag("Tile"))
-                            {
-                                toMoveTo = _hit.collider.gameObject;
-                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, -90, 0);
-                            }
-                        }
-
+                switch (_testX)
+                {
+                    case 'p':
+                        pathFinding[1] = Moves.Left;
+                        pathFinding[2] = Moves.Right;
+                        break;
+                    case 'n':
+                        pathFinding[1] = Moves.Right;
+                        pathFinding[2] = Moves.Left;
+                        break;
+                    case 'z':
+                        pathFinding[1] = Moves.Left;
+                        pathFinding[2] = Moves.Right;
                         break;
                     default:
                         break;
@@ -158,8 +115,242 @@ namespace CarterGames.NoPresentsForYou
             }
             else
             {
-                RaycastHit _hit;
+                switch (_testX)
+                {
+                    case 'p':
+                        pathFinding[0] = Moves.Left;
+                        pathFinding[3] = Moves.Right;
+                        break;
+                    case 'n':
+                        pathFinding[0] = Moves.Right;
+                        pathFinding[3] = Moves.Left;
+                        break;
+                    case 'z':
+                        pathFinding[0] = Moves.Left;
+                        pathFinding[3] = Moves.Right;
+                        break;
+                    default:
+                        break;
+                }
 
+                switch (_testZ)
+                {
+                    case 'p':
+                        pathFinding[1] = Moves.Down;
+                        pathFinding[2] = Moves.Up;
+                        break;
+                    case 'n':
+                        pathFinding[1] = Moves.Up;
+                        pathFinding[2] = Moves.Down;
+                        break;
+                    case 'z':
+                        pathFinding[1] = Moves.Down;
+                        pathFinding[2] = Moves.Up;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+
+        private bool CheckDirection(Moves toCheck)
+        {
+            switch (toCheck)
+            {
+                case Moves.Up:
+                    if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
+                    {
+                        if (_hit.collider.CompareTag("Tile"))
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                        return false;
+                case Moves.Down:
+                    if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
+                    {
+                        if (_hit.collider.CompareTag("Tile"))
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                        return false;
+                case Moves.Left:
+                    if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
+                    {
+                        if (_hit.collider.CompareTag("Tile"))
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                        return false;
+                case Moves.Right:
+                    if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
+                    {
+                        if (_hit.collider.CompareTag("Tile"))
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                        return false;
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+
+        private void MoveEnemy()
+        {
+            if (assignedMoves == null || assignedMoves.Count.Equals(0))
+            {
+                if (CheckDirection(pathFinding[0]))
+                {
+                    switch (pathFinding[0])
+                    {
+                        case Moves.Up:
+                            if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 180, 0);
+                            }
+                            break;
+                        case Moves.Down:
+                            if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Left:
+                            if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Right:
+                            if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                    }
+
+                }
+                else if (CheckDirection(pathFinding[1]))
+                {
+                    switch (pathFinding[1])
+                    {
+                        case Moves.Up:
+                            if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 180, 0);
+                            }
+                            break;
+                        case Moves.Down:
+                            if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Left:
+                            if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Right:
+                            if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                    }
+                }
+                else if (CheckDirection(pathFinding[2]))
+                {
+                    switch (pathFinding[2])
+                    {
+                        case Moves.Up:
+                            if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 180, 0);
+                            }
+                            break;
+                        case Moves.Down:
+                            if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Left:
+                            if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Right:
+                            if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                    }
+                }
+                else if (CheckDirection(pathFinding[4]))
+                {
+                    switch (pathFinding[4])
+                    {
+                        case Moves.Up:
+                            if (Physics.Raycast(transform.position, -transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 180, 0);
+                            }
+                            break;
+                        case Moves.Down:
+                            if (Physics.Raycast(transform.position, transform.forward * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Left:
+                            if (Physics.Raycast(transform.position, transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                        case Moves.Right:
+                            if (Physics.Raycast(transform.position, -transform.right * 2f, out _hit))
+                            {
+                                toMoveTo = _hit.collider.gameObject;
+                                transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, 0);
+                            }
+                            break;
+                    }
+                }
+            }
+            else
+            {
                 if (assignedMoves.Count > 0)
                 {
                     switch (assignedMoves[assignedMoves.Count - 1])
